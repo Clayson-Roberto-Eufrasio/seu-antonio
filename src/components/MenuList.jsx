82bac0c1908d1109bdfe9587ee3e007fcafe7748
Clayson R Eufrasio <clayson420eufrasio@gmail.com>
2024-12-01 08:39:59 -0300

    Removido efeito das imagens e adicionado vinhos

--- src/components/MenuList.jsx
+++ src/components/MenuList.jsx
@@ -1,12 +1,11 @@
 // src/MenuList.jsx
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import MenuItem from './MenuItem';
 
 const CardContainer = styled.div`
   background-color: transparent;
   padding: 20px;
-  transition: opacity 0.5s ease-in-out;
 `;
 
 const ListContainer = styled.div`
@@ -14,66 +13,16 @@ const ListContainer = styled.div`
 `;
 
 const ItemWrapper = styled.div`
-  opacity: 0;
-  filter: blur(15px);
-  transform: translateX(-100%);
-  transition: opacity 0.5s ease, filter 0.5s ease, transform 0.5s ease;
-
-  &.visible {
-    opacity: 1;
-    filter: blur(0);
-    transform: translateX(0);
-  }
+  /* Removidos os estilos de animação */
 `;
 
 const MenuList = ({ items }) => {
-  const refs = useRef([]); // Refs para os itens
-
-  // Função de callback para o IntersectionObserver simplificada
-  const handleIntersection = (entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("visible"); // Adiciona a classe 'visible' ao elemento
-      } else {
-        entry.target.classList.remove("visible"); // Remove a classe 'visible' ao elemento
-      }
-    });
-  };
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(handleIntersection, {
-      threshold: 0 // Define um threshold para considerar o item como visível
-    });
-
-    const elements = refs.current;
-    elements.forEach(element => {
-      if (element) {
-        observer.observe(element); // Observa cada elemento
-      }
-    });
-
-    return () => {
-      elements.forEach(element => {
-        if (element) {
-          observer.unobserve(element); // Cancela a observação ao desmontar
-        }
-      });
-    };
-  }, [items]);
-
   return (
     <CardContainer>
-      <ListContainer className='section'>
+      <ListContainer className="section">
         {items.map((item, index) => (
-          <ItemWrapper
-            ref={el => refs.current[index] = el} // Adiciona a ref ao elemento
-            key={index}
-            data-title={item.title} // Adiciona um atributo data-title para identificar o item
-          >
-            <MenuItem
-              key={index}
-              item={item}
-            />
+          <ItemWrapper key={index}>
+            <MenuItem item={item} />
           </ItemWrapper>
         ))}
       </ListContainer>
--- src/data.js
+++ src/data.js
@@ -1206,6 +1206,7 @@ export const WineTinto = [
 ]
 
 export const WineWhite = [
+  /*
   {
     title: "Becas Reserva Sauvignon Blanc",
     value: "154,00",
@@ -1214,18 +1215,33 @@ export const WineWhite = [
     description: "Uva: Sauvignon Blanc, notas de frutas tropicais e cítricas, especialmente de maracujá perfeitamente balanceado com pêssego, em boca ressalta os aromas do olfato e traz uma acidez refrescante, harmoniza com mariscos, salada poke e vegetais grelhados."
   },
   {
+    title: "Callia Chardonnay",
+    value: "160,00",
+    image_url: "",
+    state: "AR",
+    description: "Uva: Chardonnay, apresenta aromas complexos, intensamente frutados, com notas de pêssegos e mel, em boca, uma experiência plena e ampla revela-se, com distinção de sabores frutados de abacaxi e pera, harmoniza com peixes assados ou grelhados, massas com molho branco ou recheadas com legumes e carnes suínas grelhadas."
+  },
+  {
     title: "Becas Delle Venezie DOC Pinot Grigio",
     value: "169,00",
     image_url: "",
     state: "IT",
     description: "Uva: Pinot Grigio, traz um buquê intenso de aromas, com predominância de frutas como lima, lichia, limão siciliano e flores brancas como jasmim, em boca ressalta os aromas, com toque de frutas, refrescante acidez e sutil mineralidade, harmoniza com camarão empanado, burrata e frutos do mar."
   },
+  */
   {
-    title: "Callia Chardonnay",
-    value: "160,00",
+    title: "Astica Chardonnay & Chenin",
+    value: "154,00",
     image_url: "",
     state: "AR",
-    description: "Uva: Chardonnay, apresenta aromas complexos, intensamente frutados, com notas de pêssegos e mel, em boca, uma experiência plena e ampla revela-se, com distinção de sabores frutados de abacaxi e pera, harmoniza com peixes assados ou grelhados, massas com molho branco ou recheadas com legumes e carnes suínas grelhadas."
+    description: "Uva: Chardonnay e Chenin Blanc, aromas de frutas cítricas como abacaxi e maracujá, em boca leve, fresco, redondo e frutado, harmoniza com aperitivos, mariscos e queijos."
+  },
+  {
+    title: "Tantehue Sauvignon Blanc",
+    value: "154,00",
+    image_url: "",
+    state: "CL",
+    description: "Uva: Sauvignon Blanc, dono de um aroma rico em frutas com notas de morangos frescos, lichia, abacaxi e kiwi, em boca é fresco, intenso e suculento, que lembram também as frutas tropicais, harmoniza com pratos a base de frutos do mar, peixes e saladas."
   },
   {
     title: "Thera Sauvignon Blanc",
@@ -1244,6 +1260,13 @@ export const WineWhite = [
 ]
 
 export const WineRose = [
+  {
+    title: "Astica Rosé",
+    value: "154,00",
+    image_url: "",
+    state: "AR",
+    description: "Uva: Malbec, aromas de frutas vermelhas e negras, em boca leve, frutado e fresco, harmoniza com aperitivos, pratos picantes, carnes brancas e peixes."
+  },
   {
     title: "Conclave Reserva Rosé",
     value: "166,00",
